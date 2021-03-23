9698e86a7517401e0481baf9c0845a935a053e16
Florizt <1345055884@qq.com>
2021-03-23 17:57:37 +0800

    更新

--- rxmvvmlib/src/main/java/com/rx/rxmvvmlib/base/BaseDialogActivity.java
+++ rxmvvmlib/src/main/java/com/rx/rxmvvmlib/base/BaseDialogActivity.java
@@ -3,14 +3,22 @@ package com.rx.rxmvvmlib.base;
 import android.os.Bundle;
 import android.view.Gravity;
 
+import androidx.databinding.ViewDataBinding;
+
 /**
  * Created by wuwei
  * 2021/3/23
  * 佛祖保佑       永无BUG
  */
-public abstract class BaseDialogActivity extends BaseActivity {
+public abstract class BaseDialogActivity<V extends ViewDataBinding, VM extends BaseViewModel> extends BaseActivity {
+    protected V binding;
+    protected VM viewModel;
+
     @Override
     public void initView(Bundle savedInstanceState) {
+        this.binding = (V) super.binding;
+        this.viewModel = (VM) super.viewModel;
+
         getWindow().setGravity(setGravity());
         getWindow().setWindowAnimations(setWindowAnimations());
         getWindow().setDimAmount(setDimAmount());
